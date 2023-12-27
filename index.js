0266a46183e44cfb3a7a0ea4dc6e88ecbb0d2ceb
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2023-12-26 21:06:04 -0500

    change of welcome message

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let topMovies = [
 let suerfavorites = [];
 
 app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
+    res.send('Welcome to The Donkey Archive!');
 });
 
 app.get('/api/movies', (req, res) => {
@@ -39,64 +39,59 @@ app.get('/api/movies/:id', (req, res) => {
         res.status(404).json({
             error: 'Movie not found.'
         });
-    }else {
-            res.json(movie);
-        }
-    });
+    } else {
+        res.json(movie);
+    }
+});
 
-    app.get('/genres/:name', (req, res) => {
-        const genreName = req.params.name;
-        const genre = {name: genreName, description: 'Genre Description.'};
-        if(!genre) {
-            res.status(404).json({
-                error: 'Genre not found.'
-            });
-        }else {
-            res.json(genre);
-        }
-    });
+app.get('/genres/:name', (req, res) => {
+    const genreName = req.params.name;
+    const genre = { name: genreName, description: 'Genre Description.' };
+    if (!genre) {
+        res.status(404).json({
+            error: 'Genre not found.'
+        });
+    } else {
+        res.json(genre);
+    }
+});
 
-    app.post('/user/:id/favorites/:movieID', (req, res) => {
-        const movieID = req.params.id;
-        const movie = req.params.movieID;
-        if (!userFavorites[userId]) {
-            userFavorites[userId] = [];
-        }
-        userFavorites[userId].push(movie);
+app.post('/user/:id/favorites/:movieID', (req, res) => {
+    const movieID = req.params.id;
+    const movie = req.params.movieID;
+    if (!userFavorites[userId]) {
+        userFavorites[userId] = [];
+    }
+    userFavorites[userId].push(movie);
 
-        res.send('Movie ${movie} was added to favorites.');
-    });
+    res.send('Movie ${movie} was added to favorites.');
+});
 
-    app.get('/user/:id/favorites', (req, res) => {
-        const userId = req.params.id;
-        const favorites = userFavorites[userId] || [];
-        res.json(userFavorites);
-    });
+app.get('/user/:id/favorites', (req, res) => {
+    const userId = req.params.id;
+    const favorites = userFavorites[userId] || [];
+    res.json(userFavorites);
+});
 
-    app.post('/user/register', (req, res) => {
-        res.send('User registered.');
-    });
+app.post('/user/register', (req, res) => {
+    res.send('User registered.');
+});
 
-    app.post('/user/login', (req, res) => {
-        res.send('User logged in.');
-    });
+app.post('/user/login', (req, res) => {
+    res.send('User logged in.');
+});
 
-    app.post('/user/logout', (req, res) => {
-        res.send('User logged out.');
-    });
+app.post('/user/logout', (req, res) => {
+    res.send('User logged out.');
+});
 
-    app.put('/user/:id', (req, res) => {
-        res.send('User info updated.');
-    });
+app.put('/user/:id', (req, res) => {
+    res.send('User info updated.');
+});
 
-    app.delete('/user/:id', (req, res) => {
-        res.send('User deleted.');
-    });
-
-    
-
-
-
+app.delete('/user/:id', (req, res) => {
+    res.send('User deleted.');
+});
 
 
 
