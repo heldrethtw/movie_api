cdaa17dda44c06502571dfa3c77bf17f0ba43657
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-01-08 14:32:40 -0500

    adde script file to build database

---
+++ initDB.js
@@ -0,0 +1,22 @@
+const {MongoClient} = require('mongodb');
+const {MONGO_URI} = require('./config');
+const assert = require('assert');
+
+async function main() {
+    const client =  new MongoClient(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    try {
+        await client.connect();
+        console.log('connected to MongoDB!');
+        const db = client.db('donkeyDB');
+        const collection = db.collection('movies');
+        const movies = collection.find().toArray();
+        console.log(movies);
+       
+    }catch(errror){
+        console.error('An error occured connecting to MongoDB', error);
+    }finally{
+        await client.close();
+    }
+}
+
+main().catch(console.dir);
