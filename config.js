6dbf5404cea9de596bb36846bd5e3fe4c13a75e1
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-01-12 18:39:46 -0500

    added .env, ignored

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const config = {
-    TMBD_API_KEY: 'be04cb8221fcfac6d3b85d6e1da9d6'
+    TMBD_API_KEY: 'be04cb8221fcfac6d3b85d6e1da9d6',
+    MONGO_URI: 'mongodb://localhost:27017/donkeyDB'
 };
 
 module.exports = config;
