13c09c2fa4c128b378d2eb994a3853a7a366d7af
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-01-14 09:27:31 -0500

    inserted movies an d users

--- initDB.js
+++ initDB.js
@@ -1,15 +1,24 @@
 const {MongoClient} = require('mongodb');
 const {MONGO_URI} = require('./config');
-const assert = require('assert');
 
-const moviesData = require('./movies.json');``
+
+const moviesData = require('./movies.json');
+
 async function main() {
     const client = new MongoClient(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
     try {
         await client.connect();
         console.log('connected to MongoDB!');
         const db = client.db('donkeyDB');
-        const collection = db.collection('movies');
+        
+        const moviesCollection = db.collection('movies');
+        await moviesCollection.insertMany(moviesData);
+        console.log('${moviesData.length} movies inserted!');
+
+        const usersCollection = db.collection('users');
+        await usersCollection.insertMany(usersData);
+        console.log('${usersData.length} users inserted!');
+
         const movies = await collection.find().toArray();
         console.log(movies);
        
