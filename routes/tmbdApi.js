fab46d1680532addcfe0c6b52ef880e1c9a80739
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-01-15 12:03:27 -0500

    removed depricated code

--- routes/tmbdApi.js
+++ routes/tmbdApi.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
-const {TMBD_API_KEY} = require('./config');
+import { get } from 'axios';
+import { TMBD_API_KEY } from './config';
 
 async function searchMovies(title) {
-    const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMBD_API_KEY}&query=${title}`);
+    const response = await get(`https://api.themoviedb.org/3/search/movie?api_key=${TMBD_API_KEY}&query=${title}`);
     return response.data;
 }
 
 async function fetchMovieDetails(movieID) {
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}`);
+    const response = await get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}`);
     return response.data;
 }
 
-module.exports = {searchMovies, fetchMovieDetails};
+export default {searchMovies, fetchMovieDetails};
