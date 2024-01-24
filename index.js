ed4ad7bb6502384d486631a7cb258d7f7999aecc
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-01-24 13:31:32 -0500

    added CRUD

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const {User,Movie,Genre,Director} = require('./models');
 const morgan = require('morgan');
 const tmbdRoutes = require('./routes/tmbdRoutes');
 require('dotenv').config();
 
-const Models = require('./models');
-const Movie = Models.Movie;
-const User = Models.User;
-
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost 27017/donkeyDB',{ useNewUrlParser: true, useUnifiedTopology: true });
 
-
 const app = express();
-const port = 3000;
 
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/tmbd', tmbdRoutes);
 
-app.use((err, req, res, next) => {
+
+app.use((err,req,res,next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
+const port = 3000;
 app.listen(port, () => {
     console.log(`Your app is listening on port ${port}.`);
 });
--- routes/tmbdRoutes.js
+++ routes/tmbdRoutes.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const { Movie, User} = require('../models.js');
+const { Movie, User, Genre, Director} = require('../models.js');
 
-const router = express.Router();    
+const router = express.Router();   
+
+router.post('/movies', async (req, res) => {
+    try {
+        const newMovie = await Movie(req.body);
+        await newMovie.save();
+        res.status(201).json(newMovie);
+    }
+    catch (err) {
+        console.error(error);
+        res.status(500).send('Internal server error');
+    }
+});
 
 router.get('/movies', async (req, res) => {
     try{
@@ -10,6 +22,50 @@ router.get('/movies', async (req, res) => {
     } catch(err) {
         console.error(err.stack);
         res.status(500).send('Something broke!');
+    }
+});
+
+router.get('/movies', async (req, res) => {
+    try{
+        const movies = await Movie.find();
+        res.json(movies);
+    } catch(err) {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    }
+});
+
+router.get('movies/:id', async (req, res) => {
+    try{
+        const movie = await Movie.findById(req.params.id);
+        if(movie) {
+            res.json(movie);
+        } else {
+            res.status(404).send('Movie not found');
+        }
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+router.put('/movies/:id', async (req, res) => {
+    try{
+        const updateMovie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updateMovie);
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);  
+    }
+});
+
+router.delete('/movies/:id', async (req, res) => {
+    try{
+        await Movie.findByIdAndDelete(req.params.id);
+        res.send('Movie was deleted');
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
     }
 });
 
@@ -104,6 +160,121 @@ router.put('/movies/update-director-bio/:director', async (req, res) => {
     }
 });
 
+router.put('/movies/update-genre-description/:genre', async (req, res) => {
+    try{
+        const { genre } = req.params;
+        const { newDescription } = req.body;
+        await Movie.updateMany({'Genre.Name': genre}, { 'Genre.Description': newDescription });
+        res.send('Genre description updated');
+    }catch(err) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+
+router.post('/genres', async (req, res) => {
+    try {
+        const newGenre = new Genre(req.body);
+        await newGenre.save();
+        res.status(201).json(newGenre);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+
+router.get('/genres', async (req, res) => {
+    try {
+        const genres = await Genre.find();
+        res.json(genres);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+
+router.get('/genres/:id', async (req, res) => {
+    try {
+        const genre = await Genre.findById(req.params.id);
+        if (genre) {
+            res.json(genre);
+        } else {
+            res.status(404).send('Genre not found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+
+router.put('/genres/:id', async (req, res) => {
+    try {
+        const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedGenre);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+
+router.delete('/genres/:id', async (req, res) => {
+    try {
+        await Genre.findByIdAndDelete(req.params.id);
+        res.send('Genre deleted');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+router.post('/users', async (req, res) => {
+    try{
+        const existingUser = await User.findOne({ Username: req.body.Username });
+        if(existingUser) {
+            return res.status(400).send('Username already exists');
+        }
+        const newUser = new User({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+        });
+        await newUser.save();
+        res.status(201).json(newUser);
+    }catch(error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+        }
+    });
+
+router.get('/users', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    }catch(err) {
+        console.error(err);
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.get('/users/:username', async (req, res) => {
+    try{
+        const user = await User.findOne({ Username: req.params.username });
+        if(user) {
+            res.json(user);
+        } else {
+            res.status(404).send('User not found');
+        }
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
 router.put('/users/:username/add-favorite', async (req, res) => {
     try{
         const { username } = req.params;
@@ -125,6 +296,41 @@ router.delete('/users/:username', async (req, res) => {
         res.status(500).send('Internal server error');
     }
 });
+
+router.post('/directors', async (req, res) => {
+    try{
+        const directors = await Director.find();
+        res.json(directors);
+        res.status(201).json(directors);
+    }catch(err) {
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+router.get('/directors/:id', async (req, res) => {
+    try{
+        const director = await Director.findById(req.params.id);
+        if(director) {
+            res.json(director);
+        } else {
+            res.status(404).send('Director not found');
+        }
+    }catch(err) {
+        res.status(500).send('error: ' + error);
+    }
+});
+
+router.put('/directors/:id', async (req, res) => {
+    try{
+        await Director.findByIdAndDelete(req.params.id);
+        res.send('Director deleted');
+    }catch(err) {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+    }
+});
+
+
 
 module.exports = router;
 
