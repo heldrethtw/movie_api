c0b84b93e69b1fb9344905381555aae070234e64
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-02-19 04:55:25 -0500

    updated config.js to include env variables

--- config.js
+++ config.js
@@ -1,6 +1,24 @@
 // Desc: This file contains the configuration for the application
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const config = {
-    TMBD_API_KEY: process.env.TMBD_API_KEY || '',};
+    port: process.env.PORT || 3000,
+    db: {
+        uri: process.env.MONGO_URI,
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    },
+    tmbdApiKey: process.env.TMBD_API_KEY,
+    jwtSecret: process.env.JWT_SECRET,
+    cors: {
+        allowedOrigins: process.env.CORS_ALLOWED_ORIGINS ? process.env.CORS_ALLOWED_ORIGINS.split(',') : ['http://localhost:3000']
+    }
+};
+
     
   
 
--- index.js
+++ index.js
@@ -13,7 +13,8 @@ import tmbdRoutes from './routes/tmbdRoutes.js';
 
 config();
 
-connect(process.env.MONGO_URI || 'mongodb://localhost:27017/donkeyDB')
+connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB with Mongoose'))
     .catch(error => console.error('Error connecting to MongoDB:', error));
 
