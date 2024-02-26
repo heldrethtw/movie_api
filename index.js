7e85c965f5b1c32c00df64a5cf733cfa56c78273
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-02-25 20:31:42 -0500

    removed depricated usFindAndModify

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ dotenv.config();
 
 connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: true
+        useUnifiedTopology: true
+        
     }
     ).then(()=> console.log('Connected to MongoDB with Mongoose'))
     .catch(error => {
