44ece522aa624cdeb58ea38ca87a70dc6eebe0b2
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-06-08 11:02:38 -0400

    localhost 50855

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const config = {
     tmbdApiKey: process.env.TMBD_API_KEY,
     jwtSecret: process.env.JWT_SECRET,
     cors: {
-        allowedOrigins: process.env.CORS_ALLOWED_ORIGINS ? process.env.CORS_ALLOWED_ORIGINS.split(',') : ['http://localhost:3000', 'https://donkey-archive-af41e8314602.herokuapp.com', 'http://localhost:1234', 'http://localhost:49941']
+        allowedOrigins: process.env.CORS_ALLOWED_ORIGINS ? process.env.CORS_ALLOWED_ORIGINS.split(',') : ['http://localhost:3000', 'https://donkey-archive-af41e8314602.herokuapp.com', 'http://localhost:1234', 'http://localhost:49941', 'http://localhost50855']
     }
 };
 
