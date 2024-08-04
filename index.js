abf76518e4e9d9857e0cf6845cd419552e00af15
heldrethtw <141576187+heldrethtw@users.noreply.github.com>
2024-08-04 14:43:02 -0400

    added port

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(helmet());
 
 app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    origin: 'http://localhost:1234'
+    origin: 'http://localhost:1234, http://localhost:61053'
 }));
 
 app.use(morgan('common'));
